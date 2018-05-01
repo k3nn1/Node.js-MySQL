eb7e3fc495a394d058bc5d8f2b2ba552638a1704
Ken <k3nndroid@gmail.com>
2018-05-01 15:34:40 -0600

    adding files

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Node.js-MySQL
+# Bamazon using Node. JS & MySQL
+It is an Amazon-like storefront app with functionality for customers to buy products. (still in progress: managers application to update inventory in databases, and supervisors application to view cost and profits.)
+
+## Technologies
+- Visual Studio Code
+- Javascript
+- Node.js
+- MySQL Workbench
+
+## Node Package Manager
+- mysql
+- inquirer
+- cli-table
+- colors
+
+## Instructions 
+
+1. Clone repo to your local machine
+2. Install required NPM
+3. Open up MySQL Workbench to create the database and tables using the schema file
+4. Once above are done, enter node <file_name.js>
+
+### For Example: Customer App
+A table of items will be displayed and a prompt will ask "What Item ID you would like to buy?": 
+5. Enter ID (one ID only)
+Screenshot example: I chose ID 2
+(/screenshots/customer2-ex_itemID.png) 
+
+Then choose how many of the product you want to purchase. Depending on the amount you enter, 
+    - If we there is no stock left, it will say "Insufficient..." and the process will start over again.
+    - If we there is enough in stock, it will continue on and will generate your total price.
+6. Enter amount
+Screenshot: I chose amount 2
+(/screenshots/customer3-ex_amount.png) 
+
+Once everything goes through with total price, the database will update the stock inventory.
+Screenshot: Final Sale once everthing goes through
+(/screenshots/customer4-FinalSale.png) 
+
+Still in development stage:
+### Manager App
+### Supervisor App
+
+## Database Screenshots
+- Before Purchase of ITEM ID 2
+(/screenshots/database_before_purchase.png)
+
+- After Purchase of ITEM ID 2
+(/screenshots/database_after_purchase.png)
---
+++ bamazon.sql
@@ -0,0 +1,54 @@
+DROP DATABASE IF EXISTS bamazon_DB;
+CREATE DATABASE bamazon_DB;
+USE bamazon_DB;
+
+CREATE TABLE products (
+    item_id INTEGER(10) AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(100),
+    department VARCHAR(100),
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INTEGER(10) NOT NULL,
+    PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department, price, stock_quanity)
+VALUES ("drone", "electronics", 500, 20),
+("laptop", "electronics", 1500, 10),
+("javascript_book", "book", 50, 10),
+("mySql_book", "book", 40, 10),
+("snickers", "food", 1, 50),
+("monitor", "electronics", 200, 8),
+("Adidas_shoes", "clothing", 80, 10),
+("shirt", "clothing", 25, 20),
+("jeans", "clothing", 40, 20),
+("chips", "food", 2.50, 25);
+
+-- INSERT INTO products (product_name, department, price, stock_quanity)
+-- VALUES ("drone", "electronics", 500, 20);
+
+-- INSERT INTO products (product_name, department, price, stock_quanity)
+-- VALUES ("laptop", "electronics", 1500, 10);
+
+-- INSERT INTO products (product_name, department, price, stock_quanity)
+-- VALUES ("javascript_book", "book", 50, 10);
+
+-- INSERT INTO products (product_name, department, price, stock_quanity)
+-- VALUES ("mySql_book", "book", 40, 10);
+
+-- INSERT INTO products (product_name, department, price, stock_quanity)
+-- VALUES ("snickers", "food", 1, 50);
+
+-- INSERT INTO products (product_name, department, price, stock_quanity)
+-- VALUES ("monitor", "electronics", 200, 8);
+
+-- INSERT INTO products (product_name, department, price, stock_quanity)
+-- VALUES ("Adidas_shoes", "clothing", 80, 10);
+
+-- INSERT INTO products (product_name, department, price, stock_quanity)
+-- VALUES ("shirt", "clothing", 25, 20);
+
+-- INSERT INTO products (product_name, department, price, stock_quanity)
+-- VALUES ("jeans", "clothing", 40, 20);
+
+-- INSERT INTO products (product_name, department, price, stock_quanity)
+-- VALUES ("chips", "food", 2.50, 25);
